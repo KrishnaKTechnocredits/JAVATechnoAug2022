4d197d08ff5f3432d5052e896ce4be1321f40310
Hindavi16 <hindavijadhav@gmail.com>
2022-08-28 13:52:59 +0530

    Test1_Hindavi

---
+++ src/hindaviIngle/NoisDivisible3and7.java
@@ -0,0 +1,26 @@
+package hindaviIngle;
+
+public class NoisDivisible3and7 {
+
+	int count;
+
+	void noDivisibility(int startindex, int endindex) {
+		for (int index = startindex; index <= endindex; index++) {
+			if (index % 3 == 0 && index % 7 == 0) {
+				System.out.println(index + " is divisible by 3 & 7");
+				count++;
+			}
+			if (count == 3) {
+				break;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		NoisDivisible3and7 noisDivisible3and7 = new NoisDivisible3and7();
+		noisDivisible3and7.noDivisibility(20, 100);
+
+	}
+
+}
