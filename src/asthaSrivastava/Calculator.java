8d28fd962d3ea8bf3edae546a3719c328e367de5
Asthasrivas <astha.srivas1992@gmail.com>
2022-08-28 09:19:08 +0530

    Astha_Assignments

---
+++ src/asthaSrivastava/Calculator.java
@@ -0,0 +1,28 @@
+package asthaSrivastava;
+
+public class Calculator {
+	
+	void add(int x, int y, int z){
+		System.out.println("result is "+ (x+y+z));
+	}
+	
+	void sub(int x, int y, int z){
+		System.out.println("result is "+ (x-y-z));
+	}
+	
+	void mul(int x, int y,int z){
+		System.out.println("result is "+ (x*y*z));
+	}
+	
+	void div(int x, int y,int z){
+		System.out.println("result is "+ (x/y/z));
+	}
+	
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		calculator.add(100,30,10);
+		calculator.sub(250,100,50);
+		calculator.mul(40,54,10);
+		calculator.div(100,2,1);
+	}
+}		
