3461d038977efaac4b49b54c62031428eaeecb7f
Asthasrivas <astha.srivas1992@gmail.com>
2022-08-28 16:19:58 +0530

    Test1_Astha

---
+++ src/asthaSrivastava/PrintNumber.java
@@ -0,0 +1,22 @@
+package asthaSrivastava;
+
+public class PrintNumber {
+	
+	int count=0;
+	
+	void print3No(int startnum,int endnum){
+		for(int index=startnum;startnum<=endnum;index++) {
+			if (index%3==0 && index%7==0) {
+				System.out.println("Result is:"+index);
+				count++;
+			}
+			if (count>=3)
+				break;
+		}
+	}
+		
+		public static void main(String[] args) {
+			PrintNumber printNumber = new PrintNumber();
+			printNumber.print3No(20,100);
+	}
+}
