13c962fd899a109a2a7c183be6370fc4f2fc1200
Pranjali Vyavaharkar <vyavaharkarpranjali@gmail.com>
2022-08-28 09:19:20 +0530

    Assignments_1-5_PranjaliV

---
+++ src/pranjaliV/Example.java
@@ -0,0 +1,25 @@
+package pranjaliV;
+
+class Example{
+	
+	int x = 10;
+	int y = 20;
+	
+	void processData(){
+		int z = x+y;
+		x = x+10;
+		y = y+10;
+	}
+	
+	void display(){
+		System.out.println("Sum is " + x+y);
+		System.out.println(x+y + " is my answer");
+		System.out.println("Sum is " + (x+y));
+	}
+	
+	public static void main(String[] args){
+		Example example = new Example();
+		example.processData();
+		example.display();
+	}
+}
