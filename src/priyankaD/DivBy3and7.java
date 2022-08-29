154896933cf9ea2625b92a7d42dad4a6ccc38adb
PriyankaDhobale <dhobale.priyanka07@gmail.com>
2022-08-29 17:28:21 +0530

    Test1_Priyanka

---
+++ src/priyankaD/DivBy3and7.java
@@ -0,0 +1,21 @@
+package priyankaD;
+
+public class DivBy3and7 {
+	
+    void processData(int startR, int endR) {
+		int count=0;
+		for(int index=startR;index<=endR;index++) { 
+			if(index%3==0 && index%7==0) {
+				count++;
+				System.out.println(index);
+	    } 
+		 if(count==3)
+			 break;
+		 }
+	}
+
+		public static void main(String[]args) {
+			DivBy3and7 div=new DivBy3and7();
+			div.processData(20,100);
+		}
+	}
