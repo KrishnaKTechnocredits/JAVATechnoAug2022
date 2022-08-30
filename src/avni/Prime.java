d1ea50b24dd90ce1db0ce809753705bb6d990a4a
AvaniDalsaniya01 <avanipatel1672001@gmail.com>
2022-08-30 07:38:57 +0530

    Assignment_6_8_AvaniD

---
+++ src/avni/Prime.java
@@ -0,0 +1,25 @@
+class Prime{
+
+   void isPrime(int num){
+	   if(num>0){
+			boolean flag = true;
+			int n=(num/2);
+			for(int i=2;i<=n;i++){
+				if(num%i ==0){
+					flag = false;
+					break;
+				}
+			}
+		if(flag = false)
+			System.out.println("not prime");
+		else
+			System.out.println("prime");
+	   }
+	   
+    }
+	public static void main(String[] args){
+		Prime prime = new Prime();
+		prime.isPrime(17);
+		prime.isPrime(18);
+	}
+}
