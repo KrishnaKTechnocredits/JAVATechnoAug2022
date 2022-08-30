7d8208bd202dd1d53d067a1e55344c7ddece4ac5
Deepak Borse <deepak.borse27@gmail.com>
2022-08-30 08:52:19 +0530

    [200~ProgrammingTest1.java
    q

--- src/deepakBorse/ProgrammingTest1.java
+++ src/deepakBorse/ProgrammingTest1.java
@@ -13,26 +13,40 @@ Note : Use for loop*/
 package deepakBorse;
 
 public class ProgrammingTest1 {
+
 	
 	void processData(int startR, int endR) {
-		int index=0, count1=0;
-		for(index=startR;index<=endR;index++)
-		{
-			if(index%3==0 && index%7==0) {
+		int index = 0, count1 = 0;
+		for (index = startR; index <= endR; index++) {
+			if (index % 3 == 0 && index % 7 == 0) {
 				System.out.println(index);
 				count1++;
-			}	
-			if(count1==3)
+			}
+			if (count1 == 3)
 				break;
 		}
-		
+
+	}
+	 
+	void processDataR(int startR, int endR) {
+		int count1 = 0, index;
+		System.out.println("Number Divisible by 3 & 7 in reverse order from "+endR+" to "+startR);
+		for (index = endR; index>= startR; index--) {
+			if (index % 3 == 0 && index % 7 == 0) {
+				System.out.println(index);
+				count1++;
+			}
+			if (count1 == 3)
+				break;
+		}
+
 	}
 
 	public static void main(String[] args) {
-		ProgrammingTest1 ProgrammingTest1=new ProgrammingTest1();
-		
-		ProgrammingTest1.processData(20, 100);
+		ProgrammingTest1 ProgrammingTest1 = new ProgrammingTest1();
 
+		ProgrammingTest1.processData(20, 100);
+		ProgrammingTest1.processDataR(20,100);
 	}
 
 }
