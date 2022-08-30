cb8ca460faceaef30ca05c4017d7ba6b093f860a
Deepak <deepak.patil793@gmail.com>
2022-08-30 18:51:37 +0530

    Assignment 10

---
+++ src/deepakPatil/LoopBreakAssignment.java
@@ -0,0 +1,31 @@
+package deepakPatil;
+
+/*Assignment - 10 : 30th Aug'2022
+Find last 3 numbers divisible by 3 and 7.
+
+Input : 10 to 200
+Output : 189
+              168
+              147   
+*/
+
+public class LoopBreakAssignment {
+
+	void findNumberDivisibleby3and7(int startIndex, int endIndex) {
+		int count = 0;
+		for (int i = endIndex; i > startIndex; i--) {
+			if (i % 3 == 0 && i % 7 == 0) {
+				System.out.println(i);
+				count++;
+			}
+			if (count == 3) {
+				break;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new LoopBreakAssignment().findNumberDivisibleby3and7(10, 200);
+	}
+
+}
