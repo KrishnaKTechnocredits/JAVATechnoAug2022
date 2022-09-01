4e3c2a328030b504ac00fafcd4f26d5a423ac9e9
Swapnil Maheshwari <swapnilmaheshwari5@outlook.com>
2022-09-01 22:36:04 +0530

    Assignment 12 submitted

---
+++ src/swapnilMaheshwari/PrimeNumInRangeOptimized.java
@@ -0,0 +1,40 @@
+package swapnilMaheshwari;
+
+public class PrimeNumInRangeOptimized {
+	int count = 0, sum = 0, avg;
+
+	void primeNumber(int num) {
+		
+			boolean status = true;
+			for (int i = 2; i <num; i++) {
+				if (num % i == 0) {
+					// System.out.println(num + " is not a prime number\n");
+					status = false;
+					break;
+				}
+			
+			}
+			if (status == true) {
+				System.out.println(num + " is Prime Number");
+				count++;
+				sum = sum + num;
+			status = true;
+		}
+	}
+
+	void primeOptimized(int startRange, int endRange) {
+			for (int i = startRange; i < endRange; i++) {
+				primeNumber(i);
+			}
+			System.out.println("Total count of prime number is" + count);
+			avg = sum / count;
+			System.out.println("Average of total count" + avg);
+		} 
+
+	public static void main(String[] args) {
+		PrimeNumInRangeOptimized primeNumInRangeOptimized = new PrimeNumInRangeOptimized();
+		// primeNumInRangeOptimized.primeNumber(11);
+		primeNumInRangeOptimized.primeOptimized(100, 120);
+	}
+
+}
