71223b4c58a34f4b1ac048335e79939c501814c9
grvs4893@gmail.com <grvs4893@gmail.com>
2022-09-02 16:07:24 +0530

    Prime Number Assignment Count , Avg, Sum of prime number in given Range

--- src/gauravSahu/NumberSystem/Prime2.java
+++ src/gauravSahu/NumberSystem/Prime2.java
@@ -2,10 +2,10 @@ package gauravSahu.NumberSystem;
 
 public class Prime2 {
 
-	static int count; 
+	static int count;
 	static int sum;
 	static float avg;
-	
+
 	void checkprime(int num) {
 		boolean flag = true;
 		for (int index = 2; index <= num / 2; index++) {
@@ -13,14 +13,14 @@ public class Prime2 {
 				flag = false;
 				break;
 			}
-		}if (flag == true) {
-			System.out.println(num + "  is a prime number" );
-				count++;
-				sum = sum + num;	
-				avg = sum/count;
-		} 
+		}
+		if (flag == true) {
+			System.out.println(num + "  is a prime number");
+			count++;
+			sum = sum + num;
+			avg = sum / count;
+		}
 	}
-	
 
 	void checknum(int startnum, int endnum) {
 		for (int num = startnum; num <= endnum; num++) {
@@ -31,8 +31,7 @@ public class Prime2 {
 	public static void main(String[] args) {
 		new Prime2().checknum(10, 100);
 		System.out.println("Total prime number is " + count);
-		System.out.println("Total sum of prime numbers " + sum );
-		
-		System.out.println("Average of prime numbers " + avg );
+		System.out.println("Total sum of prime numbers " + sum);
+		System.out.println("Average of prime numbers " + avg);
 	}
 }
