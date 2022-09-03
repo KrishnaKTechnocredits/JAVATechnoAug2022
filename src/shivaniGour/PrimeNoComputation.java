f86a446cf588245a215d97eb53f93f9c5e84231f
Shivani Gour <shivanigour31@gmail.com>
2022-09-03 22:31:57 +0530

    Assignment 12 Shivani

--- src/shivaniGour/PrimeNoComputation.java
+++ src/shivaniGour/PrimeNoComputation.java
@@ -2,6 +2,8 @@ package shivaniGour;
 
 public class PrimeNoComputation {
 	int countPrime;
+    int sum;
+    float avg;
 	void prime(int num) {
 		boolean flag = false;
 		for (int i = 2; i <= num / 2; i++) {
@@ -10,16 +12,21 @@ public class PrimeNoComputation {
 				break;
 			}
 		}
-		if (!flag)
+		if (!flag) {
 			System.out.println(num + " is a Prime Number");
-		    countPrime++;
+			sum=sum+num;
+			countPrime++;
+		}
 	}
-	
+
 	void primeRange(int startRange, int endRange) {
 		for (int n = startRange; n <= endRange; n++) {
 			prime(n);
 		}
-		System.out.println("Total number of Prime Numbers"+countPrime);
+		avg = (float)sum/countPrime;
+		System.out.println("Total number of Prime Numbers: " +countPrime);
+		System.out.println("Sum of Prime Numbers: " +sum);
+		System.out.println("Average of Prime Numbers: " +avg);	
 	}
 
 	public static void main(String[] args) {
