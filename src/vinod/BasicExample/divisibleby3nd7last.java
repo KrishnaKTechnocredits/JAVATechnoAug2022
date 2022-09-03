06a0ec20425b0cb1ffc1f35dfd323a4eb0bda1e3
vinod yadav <vinody242@gmail.com>
2022-09-03 20:30:32 +0530

    last digit prog

---
+++ src/vinod/BasicExample/divisibleby3nd7last.java
@@ -0,0 +1,22 @@
+package vinod.BasicExample;
+
+public class divisibleby3nd7last {
+
+	void divisibleBy3And7(int num1, int num2) {
+		System.out.print("Numbers divisible by 3 and 7 between " +num1+ " to " +num2+ " from last are: ");
+		int count = 0;
+		for (int i = num2; i >= num1; i--) {
+			if (i % 3 == 0 && i % 7 == 0) {
+				System.out.print(i + ", ");
+				count++;
+			}
+			if (count == 4)
+				break;
+		}
+	}
+
+	public static void main(String[] args) {
+		new divisibleby3nd7last().divisibleBy3And7(1, 200);
+	}
+
+}
