477132bb32e9fdc9607c35add42cf07a88a87954
AkankshaJava <akankshavyasjava@gmail.com>
2022-09-03 19:40:50 +0530

    notes added

--- src/akankshaVyas/forLoopPrimeBanking/PrintPrimeNumByWhileLoop.java
+++ src/akankshaVyas/forLoopPrimeBanking/PrintPrimeNumByWhileLoop.java
@@ -1,3 +1,22 @@
+/* Created counting using while loop and also write method to find prime number using for and while loop
+ * /*
+Assignment - 14 : 2nd Sep'2022
+Using While loop
+
+*Program 1: *
+Print first N prime numbers.
+N should be passed as parameter in the method.
+Hint : 5 pass as parameter, method should print first 5 prime numbers.
+
+sample input : 5
+output : 2
+              3
+              5
+              7
+              11
+*/
+
+
 package akankshaVyas.forLoopPrimeBanking;
 
 public class PrintPrimeNumByWhileLoop {
