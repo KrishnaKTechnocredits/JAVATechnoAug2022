3735180d46fab756974d231f21123a7a23f77c86
Pranjali Vyavaharkar <vyavaharkarpranjali@gmail.com>
2022-09-04 13:52:49 +0530

    Test2_Pranjali

--- src/pranjaliV/Test2/RequiredNumbers.java
+++ src/pranjaliV/Test2/RequiredNumbers.java
@@ -1,3 +1,25 @@
+/*Test - 2: 4th Sep'2022
+
+void findRequiredNumbers(int targetNum){
+	int count = 0;
+	while(sum<targetNum){		
+		count++;
+	}
+	sop(count);
+}
+
+-----------------------------
+
+findRequiredNumbers(50);
+
+input : 50
+output : 10
+
+input : 17
+output : 6
+
+1+2+3+4+5+6+7+8+9+10 = 55*/
+
 package pranjaliV.Test2;
 
 public class RequiredNumbers {
