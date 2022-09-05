aeabf9d1dace5b01278fa44eecc7016075833b92
Pranjali Vyavaharkar <vyavaharkarpranjali@gmail.com>
2022-09-05 23:23:31 +0530

    Assignment15_Pranjali

--- src/pranjaliV/assignment15/PrimeNumber.java
+++ src/pranjaliV/assignment15/PrimeNumber.java
@@ -1,17 +1,8 @@
-/*Assignment - 14 : 2nd Sep'2022*
-Using While loop
-
-*Program 1: *
-Print first N prime numbers.
-N should be passed as parameter in the method.
-Hint : 5 pass as parameter, method should print first 5 prime numbers.
-
-sample input : 5
-output : 2
-              3
-              5
-              7
-              11*/
+/*Assignment - 15 : 5th Sep'2022*
+	we need to do summ of first 5 prime number, 
+	using return keyword
+	output: (2+3+5+7+11) = 28
+*/
               
 package pranjaliV.assignment15;
 
