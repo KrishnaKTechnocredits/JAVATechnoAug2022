c70fc5b3a51647c3f92d3e619843b4de0ca0455d
harshvegada <harshhpatel07@gmail.com>
2022-09-05 10:03:49 +0530

    added example for return type

---
+++ src/technocredits/basicexamples/returnType/PrimeNumber.java
@@ -0,0 +1,19 @@
+package technocredits.basicexamples.returnType;
+
+public class PrimeNumber {
+
+	boolean printNPrimeNumbers(int n) {
+		int count = 0;
+		int num = 2;
+	}
+
+	static void m1(int start, int end) {
+		for (int i = start; i < end; i++) {
+			printNPrimeNumbers(i);
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
