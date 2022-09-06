b7f3f3c0101562e34a77c92e5570029214f5d7a9
Harshal Rane <harshal.rane@siemens.com>
2022-09-06 07:29:39 +0530

    Assignment15_Harshal_MainMethodUpdated

--- src/harshalRane/Assignments/FirstNPRimeNumbersWithReturn.java
+++ src/harshalRane/Assignments/FirstNPRimeNumbersWithReturn.java
@@ -6,40 +6,41 @@ Sum of first 5 prime number, using return keyword
 output: (2 + 3 + 5 + 7 + 11) = 28
 */
 
-class FirstNPRimeNumbersWithReturn{
-	int count=0;
-	static int sum =0;
-	
-	boolean isPrime(int num){
+class FirstNPRimeNumbersWithReturn {
+	int count = 0;
+	static int sum = 0;
+
+	boolean isPrime(int num) {
 		int index = 2;
-		boolean flag=true;
-		
-		while(index<=num/2){
-			if(num%index==0){
-				flag=false;
+		boolean flag = true;
+
+		while (index <= num / 2) {
+			if (num % index == 0) {
+				flag = false;
 				break;
 			}
 			index++;
 		}
-		if(flag){
+		if (flag) {
 			count++;
-			sum = sum+num;
+			sum = sum + num;
 		}
 		return true;
 	}
-		
-		int firstNPrime(int number){
-		int num=2;
-		System.out.println("Sum of First "+number+" prime numbers is: ");
-		while(count<number){
+
+	int firstNPrime(int number) {
+		int num = 2;
+		System.out.println("Sum of First " + number + " prime numbers is: ");
+		while (count < number) {
 			isPrime(num);
 			num++;
 		}
 		return sum;
-		}
-		
-		public static void main(String[] args){
-			new FirstNPRimeNumbersWithReturn().firstNPrime(5);
-			System.out.println(sum);
-		}
 	}
+
+	public static void main(String[] args) {
+		FirstNPRimeNumbersWithReturn obj = new FirstNPRimeNumbersWithReturn();
+		int result = obj.firstNPrime(5);
+		System.out.println(result);
+	}
+}
