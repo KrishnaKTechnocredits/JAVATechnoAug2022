aa0473d31ec9723c8d6b0a80c98dd87ed9c1fbe1
AnujaDevangare <devangareanuja96@gmail.com>
2022-09-06 21:17:52 +0530

    Assignment14_Anuja_Modified

--- src/anujaD/LoopExample.java
+++ src/anujaD/LoopExample.java
@@ -17,5 +17,33 @@ output : 2
 package anujaD;
 
 public class LoopExample {
+	int count;
 
+	void primeNumbers(int num) {
+		boolean flag = true;
+		for (int Index = 2; Index <= num / 2; Index++) {
+			if (num % Index == 0) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag == true) {
+			count++;
+			System.out.println(num);
+		}
+	}
+	void printPrimeNumber(int number) {
+		int num =2;
+		System.out.println("Print first N prime numbers: ");
+		
+		while(num>=2 && count !=number) {
+			primeNumbers(num);
+			num++;
+			
+		}
+	}
+	public static void main(String[] args) {
+		new LoopExample().printPrimeNumber(5);
+		
+	}
 }
