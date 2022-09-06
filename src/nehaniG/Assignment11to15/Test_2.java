8af9c04ca5478ad5a99b629849991dc32809c9ef
Nehani <nehani.gupta@gmail.com>
2022-09-06 12:01:08 +0530

    Assignment16_NehaniG

---
+++ src/nehaniG/Assignment11to15/Test_2.java
@@ -0,0 +1,20 @@
+package nehaniG.Assignment11to15;
+
+public class Test_2 {
+	void findRequiredNumber(int targetNum) {
+		int count = 0;
+		int sum = 0;
+		while (sum< targetNum) {
+		count++;
+		sum = sum+count;
+		}
+		System.out.println(count);
+	}
+	public static void main(String[] args) {
+		Test_2 test_2 = new Test_2();
+		test_2.findRequiredNumber(50);
+		test_2.findRequiredNumber(17);
+		
+				
+	}
+}
