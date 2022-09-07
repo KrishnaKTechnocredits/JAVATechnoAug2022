fdb0fca234880aee0b42d1095fbcf1ccde09dcb5
Sohail Mujawar <sohailexi@gmail.com>
2022-09-07 22:32:53 +0530

    Assignment_17_Sohail

--- src/sohail/loop_program/UserDefinedRange.java
+++ src/sohail/loop_program/UserDefinedRange.java
@@ -2,18 +2,12 @@ package sohail.loop_program;
 
 class UserDefinedRange{
 
-	static void divide(int llimit, int ulimit){
-		for(int i=llimit; i<=ulimit; i++){
-			int num = 0;
-			if (num %3==0 && num%7==0){
-				System.out.println("Number is divisable by 3 and 7");
-			}
-		}
-	
-	}
+	 
 
 public static void main(String[] args){
-		UserDefinedRange.divide(30,60);
+		int a = 3, b = 8, c=6, d=5;
+		int e=--b + ++d - a-- + ++c;   //7 + 6 - 3 + 7      14 - 10
+		System.out.println(c);
 		
 }
 
