e9681421af5cf5b916a555dd9d0350d2efbdbd2a
Puja Sah <pujasah5@gmail.com>
2022-09-08 18:17:42 +0530

    Same output but implemented differently

--- src/pujaSah/assignments11_20/OperationsOnStringClassMethods.java
+++ src/pujaSah/assignments11_20/OperationsOnStringClassMethods.java
@@ -4,53 +4,42 @@ public class OperationsOnStringClassMethods {
 
 	String getArrangedStringFormat1(String input) {
 		int length = input.length();
-		char ch = 'p';
 		String str1 = "", str2 = "";
-		boolean flag = true;
 		for (int i = 0; i <= length - 1; i++) {
-			ch = input.charAt(i);
-			flag = Character.isDigit(ch);
-			if (flag) {
+			if (Character.isDigit(input.charAt(i))) {
 				str1 = str1 + input.charAt(i);
-			} else {
-				flag = Character.isLetter(ch);
-				if (flag) {
-					str2 += input.charAt(i);
-				}
+			} else if (Character.isLetter(input.charAt(i))) {
+				str2 += input.charAt(i);
 			}
 		}
-		return (str1 + str2);
+		return str1 + str2;
 	}
 
 	String getArrangedStringFormat2(String input) {
 		int length = input.length();
 		String str1 = "", str2 = "", str3 = "";
 		for (int i = 0; i <= length - 1; i++) {
-			boolean flag = Character.isLetter(input.charAt(i));
-			if (flag) {
+			if (Character.isLetter(input.charAt(i))) {
 				if (Character.isUpperCase(input.charAt(i))) {
 					str1 += input.charAt(i);
-				} else {
+				} else if (Character.isLowerCase(input.charAt(i))) {
 					str3 += input.charAt(i);
 				}
 			} else if (Character.isDigit(input.charAt(i))) {
 				str2 += input.charAt(i);
 			}
 		}
-		return (str1 + str2 + str3);
+		return str1 + str2 + str3;
 	}
 
 	void displayArrangedStringOutput(String type, String name) {
 		if (type == "AllDigitAllCharacter") {
-			String str1 = new OperationsOnStringClassMethods().getArrangedStringFormat1(name);
-			System.out.println(
-					"The arranged String in the format AllDigitAllCharacter for input " + name + " is " + str1);
-		} else if (type == "UpperCaseLettersDigitsLoweCaseLetters") {
-			String str2 = new OperationsOnStringClassMethods().getArrangedStringFormat2(name);
+			System.out.println("The arranged String in the format AllDigitAllCharacter for input " + name + " is "
+					+ new OperationsOnStringClassMethods().getArrangedStringFormat1(name));
+		}
+		if (type == "UpperCaseLettersDigitsLoweCaseLetters") {
 			System.out.println("The arranged String in the format UpperCaseLettersDigitsLoweCaseLetters for input "
-					+ name + " is " + str2);
-		} else {
-			System.out.println("Invalid Format");
+					+ name + " is " + new OperationsOnStringClassMethods().getArrangedStringFormat2(name));
 		}
 	}
 
