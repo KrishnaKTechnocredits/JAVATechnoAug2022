2e1df0de40bef253d19f9a2b7583f844ccb60503
Puja Sah <pujasah5@gmail.com>
2022-09-10 18:59:23 +0530

    modified indentation

--- src/pujaSah/assignments11_20/middleCharInEachStringOfAnArray.java
+++ src/pujaSah/assignments11_20/middleCharInEachStringOfAnArray.java
@@ -21,7 +21,6 @@ public class middleCharInEachStringOfAnArray {
 		int size = inArr.length;
 		char[] outArr = new char[size];
 		outArr = getMiddleCharOfEachString(inArr);
-
 		for (int i = 0; i < size - 1; i++) {
 			System.out.println(inArr[i] + "->" + outArr[i]);
 		}
