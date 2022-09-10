44d3e5d36c538a291aca05b5584e034cb28d2572
unknown <komald08@gmail.com>
2022-09-10 17:27:37 +0530

    Test3_Komal

--- src/komalShrivastava/Test3b.java
+++ src/komalShrivastava/Test3b.java
@@ -5,17 +5,22 @@ package komalShrivastava;
 import java.util.Arrays;
 
 public class Test3b {
+	
+	char[] getMiddleCharacter(String[] input) {
+		Test3a test3a = new Test3a();
+		char ch[] = new char[input.length];
+		for(int index=0; index <input.length; index++) {
+			ch[index] = test3a.getMiddleCharacter(input[index]);
+		}
+		return ch;
+	}
 
 	public static void main(String[] args) {
-		
-		//access getMiddleCharacter Method from Class Test3a
-		
-		Test3a test3a = new Test3a();
+		Test3b test3b = new Test3b();
 		String []inputArray = {"Mahesh","Avni","Puja","Harshal","Aditty","Rahul"};
 		System.out.println("Middle Character for each array element \n" + Arrays.toString(inputArray));
-		for(int index=0; index<inputArray.length; index++) {
-			char ch = test3a.getMiddleCharacter(inputArray[index]);
-			System.out.println(inputArray[index] + " --> " + ch);
+		char midCharArray[] = test3b.getMiddleCharacter(inputArray);
+		for(int index=0; index<inputArray.length; index++)
+			System.out.println(inputArray[index] + " --> " + midCharArray[index]);
 		}
 	}
-}
