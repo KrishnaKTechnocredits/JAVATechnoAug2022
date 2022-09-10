ca37e3bac195d6520eb1dc40734a9548da124423
grvs4893@gmail.com <grvs4893@gmail.com>
2022-09-11 00:15:30 +0530

    Assignment_Test1_Test2_20

---
+++ src/gauravSahu/Array20Test2.java
@@ -0,0 +1,38 @@
+package gauravSahu;
+
+public class Array20Test2 {
+
+	char getmiddlechar(String name) {
+		int length = name.length();
+		int index = 0;
+		if (length % 2 == 0) {
+			index = length / 2 - 1;
+		} else {
+			index = length;
+		}
+		return name.charAt(index);
+	}
+
+	char[] getString(String[] name) {
+		int length = name.length;
+		char[] charstr = new char[length];
+		for (int index = 0; index < length; index++) {
+			charstr[index] = getmiddlechar(name[index]);
+		}
+		return charstr;
+	}
+
+	public static void main(String[] args) {
+		Array20Test2 a1 = new Array20Test2();
+		String[] name = { "Mahesh", "Avni", "Puja", "Harshal", "Aditty", "Rahul" };
+		char[] name1 = a1.getString(name);
+		
+	}
+
+}
+
+/*
+ * Write a method to return middle character of each elements from given array.
+ * input : {"Mahesh","Avni","Puja","Harshal","Aditty","Rahul"} output : Mahesh
+ * -> h Avni -> v Harshal -> s Aditty -> i Rahul -> h
+ */
