a08e68d26163606d125560bb7ac04431affdd0bf
ManjiriC0406 <manjiri.chourikar@gmail.com>
2022-09-10 18:39:43 +0530

    Test3-Manjiri

---
+++ src/manjiri/Test/Test3.java
@@ -0,0 +1,39 @@
+/*Return middle character of max length String from given array.
+input : {"Krishna","Maulik","Aashvi","Technocredits","Harsh"}
+output : c*/
+
+package manjiri.Test;
+
+public class Test3 {
+	String getMaxLengthString(String[] input) {
+		int maxLength = input[0].length();
+		String maxString = input[0];
+		
+		for(int index = 1; index < input.length;index ++) {
+			int length = input[index].length();
+			if (maxLength < length) {
+				maxLength = length;
+				maxString = input[index];				
+			}
+		}
+		System.out.println(maxString);
+		return maxString;
+	}
+	
+	char getMiddleChar(String[] inputArr) {
+		String input = getMaxLengthString(inputArr);
+		
+		if (input.length() % 2 == 0) {
+			return input.charAt(input.length()/2-1);		
+		}
+		else {
+			return input.charAt(input.length()/2);
+		}		
+	}
+	
+	public static void main(String[] args) {
+		Test3 test3 = new Test3();
+		String[] stringArray = {"Krishna","Maulik","Aashvi","Technocredits","Harsh"};
+		System.out.println(test3.getMiddleChar(stringArray));
+	}
+}
