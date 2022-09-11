a4db7f794a46e7a78c41462bc0b802bdb317a8f6
shubgpta18 <shubgpta1995@gmail.com>
2022-09-11 11:58:46 +0530

    Test4_Shubham_Adding question as comments

--- src/shubhamG/Test4.java
+++ src/shubhamG/Test4.java
@@ -2,6 +2,15 @@ package shubhamG;
 
 import java.util.Arrays;
 
+/*Write a method to return sum of all the digits present in each elements from given array.
+input : {"Ma1he4sh","A9vni","P1uj3a5","Harshal","A4d4itty","Rahul"}
+output : Mahesh -> 5
+         Avni -> 9
+         Puja -> 9
+         Harshal -> 0
+         Aditty -> 8
+         Rahul -> 0
+*/         
 public class Test4 {
 
 	int getSumOfIntegersFromArray(String str) {
