4c21c9d1d3ece6eb4a24d3265c9f8b708a64aa9c
Sagar Yadav <sgr.51291@gmail.com>
2022-09-11 13:32:32 +0530

    Test3_sagarY1009 Both programs in one file

--- src/sagarY/Arrays/ArrayReturn1.java
+++ src/sagarY/Arrays/ArrayReturn1.java
@@ -4,6 +4,33 @@ import java.util.Arrays;
 
 public class ArrayReturn1 {
 
+	char getMiddleChar(String name) {
+		char ch = ' ';
+
+		if (name.length() % 2 == 0)
+			System.out.println("Middle character of the string is: " + name.charAt(name.length() / 2 - 1));
+		else
+			System.out.println("Middle character of the string is: " + name.charAt(name.length() / 2));
+
+		return ch;
+	}
+
+	String maxLengthName(String[] name) {
+
+		int stringLength = name[0].length();
+		String maxLengthName = "";
+
+		for (int index = 1; index < name.length; index++) {
+			if (stringLength < name[index].length()) {
+				stringLength = name[index].length();
+				maxLengthName = name[index];
+
+			}
+		}
+		System.out.println("Maximum Length String is: " + maxLengthName);
+		return maxLengthName;
+	}
+
 	char[] getMiddleChar(String[] inputArray) {
 		int startIndex = 0;
 		char[] outputArray = new char[inputArray.length];
@@ -22,13 +49,21 @@ public class ArrayReturn1 {
 	}
 
 	public static void main(String[] args) {
+		System.out.println("Program 1:");
+		String[] name = { "Krishna", "Maulik", "Aashvi", "Technocredits", "Harsh" };
+		System.out.println("Input Array - " + Arrays.toString(name));
+		String str = new ArrayReturn1().maxLengthName(name);
+		char ch1 = new ArrayReturn1().getMiddleChar(str);
+		
+		System.out.println("\nProgram 2:");
 		ArrayReturn1 array = new ArrayReturn1();
-		String[] arr1 = { "Krishna","Maulik","Aashvi","Technocredits","Harsh" };
+		String[] arr1 = { "Krishna", "Maulik", "Aashvi", "Technocredits", "Harsh" };
 		char[] ch = array.getMiddleChar(arr1);
 		System.out.println("Input Array - " + Arrays.toString(arr1));
 		System.out.println("Output Array with Middle name - ");
 		for (int index = 0; index < arr1.length; index++) {
 			System.out.println(arr1[index] + "- " + ch[index]);
 		}
+			
 	}
 }
