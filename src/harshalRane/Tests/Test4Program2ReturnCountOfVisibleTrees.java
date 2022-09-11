744a18b8d271d98f53551e59653c120bcf2f09a7
Harshal Rane <harshal.rane@siemens.com>
2022-09-11 15:34:03 +0530

    Test4_Harshal_Modified_Program2

--- src/harshalRane/Tests/Test4Program2ReturnCountOfVisibleTrees.java
+++ src/harshalRane/Tests/Test4Program2ReturnCountOfVisibleTrees.java
@@ -1,10 +1,12 @@
 package harshalRane.Tests;
 
+import java.util.Arrays;
+
 public class Test4Program2ReturnCountOfVisibleTrees {
 	int returnCount(int[] number) {
-		int count = 0;
-		int maxNum = 0;
-		for (int index = 0; index < number.length; index++) {
+		int count = 1;
+		int maxNum = number[0];
+		for (int index = 1; index < number.length; index++) {
 			if (maxNum < number[index]) {
 				maxNum = number[index];
 				count++;
@@ -17,7 +19,8 @@ public class Test4Program2ReturnCountOfVisibleTrees {
 		Test4Program2ReturnCountOfVisibleTrees test4 = new Test4Program2ReturnCountOfVisibleTrees();
 		int[] input1 = { 3, 7, 5 };
 		int[] input2 = { 3, 5, 11, 9, 10, 13 };
-		System.out.println(test4.returnCount(input1));
-		System.out.println(test4.returnCount(input2));
+		System.out.println(Arrays.toString(input1)+ "\n" +test4.returnCount(input1));
+		System.out.println("------------------------");
+		System.out.println(Arrays.toString(input2)+ "\n" +test4.returnCount(input2));
 	}
 }
