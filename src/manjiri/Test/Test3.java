03804dc00a0de3179860e658e503bed51916e935
ManjiriC0406 <manjiri.chourikar@gmail.com>
2022-09-11 20:43:11 +0530

    modified code with modular approach and in multiple classes

--- src/manjiri/Test/Test3.java
+++ src/manjiri/Test/Test3.java
@@ -5,7 +5,7 @@ output : c*/
 package manjiri.Test;
 
 public class Test3 {
-	String getMaxLengthString(String[] input) {
+	private String getMaxLengthString(String[] input) {
 		int maxLength = input[0].length();
 		String maxString = input[0];
 		
@@ -19,20 +19,17 @@ public class Test3 {
 		return maxString;
 	}
 	
-	char getMiddleChar(String[] inputArr) {
-		String input = getMaxLengthString(inputArr);
-		
+	private char getMiddleChar(String input) {
 		if (input.length() % 2 == 0) {
 			return input.charAt(input.length()/2-1);		
 		}
 		else {
 			return input.charAt(input.length()/2);
-		}		
+		}	
 	}
 	
-	public static void main(String[] args) {
-		Test3 test3 = new Test3();
-		String[] stringArray = {"Krishna","Maulik","Aashvi","Technocredits","Harsh"};
-		System.out.println(test3.getMiddleChar(stringArray));
+	char getMiddleCharFromMaxLengthString(String[] inputArr) {
+		String input = getMaxLengthString(inputArr);
+		return getMiddleChar(input);
 	}
 }
