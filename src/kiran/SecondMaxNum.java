ba7e3cfa2b254d32df484622aed61a2573c5df22
kalokhekk <kalokhekk@gmail.com>
2022-09-14 19:20:35 +0530

    Assignment21_1_kiran

---
+++ src/kiran/SecondMaxNum.java
@@ -0,0 +1,24 @@
+package kiran;
+
+public class SecondMaxNum {
+    int getsecondHighestNumber(int[] arr) {
+   	 int max=0;
+   	 int smax=0;
+   	 for(int i=0;i<arr.length;i++) {
+   		 if(arr[i]>max) {
+   			 max=arr[i];
+   		 }
+   		 else if(arr[i]>smax) {
+   			 smax=arr[i];
+   		 }
+   		
+   			 
+   	 }return smax;
+    }
+	public static void main(String[] args) {
+		SecondMaxNum obj=new SecondMaxNum();
+		int[] a1=new int[] {10,99,33,44,89,76};
+		System.out.println(obj.getsecondHighestNumber(a1));
+	}
+
+}
