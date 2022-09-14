229f5eab1ba4f16fdcf72abde1bd8142a854e615
Harshal Rane <harshal.rane@siemens.com>
2022-09-14 10:04:56 +0530

    Assignment21_Harshal

---
+++ src/harshalRane/Assignments/assignment21SecondMaxNumArray/SecondMaxNumInArray.java
@@ -0,0 +1,27 @@
+package harshalRane.Assignments.assignment21SecondMaxNumArray;
+
+public class SecondMaxNumInArray {
+	
+	int getMaxNum(int[] num) {
+		int maxNum=num[0];
+		int secondMaxNum=num[1];
+		
+		for(int index=1;index<num.length;index++) {
+			if(num[index]>maxNum) {
+				secondMaxNum= maxNum;
+				maxNum = num[index];
+			}
+			else if(num[index] > secondMaxNum && num[index] < maxNum) {
+				secondMaxNum = num[index];
+			}		
+		}
+		return secondMaxNum;
+	}
+	
+	public static void main(String[] args) {
+		SecondMaxNumInArray a = new SecondMaxNumInArray();
+		int[] input = {10,19,76,22,44,81,23};
+		int output = a.getMaxNum(input);
+		System.out.println(output);
+	}
+}
