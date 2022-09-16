0f65863a5d0955c0055706a99cc32f5e4771547f
Puja Sah <pujasah5@gmail.com>
2022-09-16 23:01:14 +0530

    Please review Assignment_23_PujaSah_16Sep

---
+++ src/pujaSah/assignments20_30/MaxLengthWord.java
@@ -0,0 +1,24 @@
+package pujaSah.assignments20_30;
+
+public class MaxLengthWord {
+
+	String getMaxLengthWord(String inStr) {
+		String[] outArr = inStr.split(" ");
+		String maxLengthWord = outArr[0];
+		for (int index = 1; index < outArr.length; index++) {
+			if (outArr[index].length() > maxLengthWord.length()) {
+				maxLengthWord = outArr[index];
+			}
+		}
+		return maxLengthWord;
+	}
+
+	public static void main(String[] args) {
+		MaxLengthWord obj = new MaxLengthWord();
+
+		String inputString = "Good morning friends this is technocredits";
+		System.out.println("InputString: " + inputString + "\n");
+		
+		System.out.println("The maximum length word is: " + obj.getMaxLengthWord(inputString));
+	}
+}
---
+++ src/pujaSah/assignments20_30/MaxUpperCaseWord.java
@@ -0,0 +1,30 @@
+package pujaSah.assignments20_30;
+
+public class MaxUpperCaseWord {
+
+	String getWordWithMaxUpperCaseLetters(String inArr) {
+		String[] outArr = inArr.split(" ");
+		String maxUpperCaseWord = " ";
+		int maxCount = 0;
+		for (int i = 0; i < outArr.length; i++) {
+			int count = 0;
+			for (int j = 0; j <= outArr[i].length() - 1; j++) {
+				if (Character.isUpperCase(outArr[i].charAt(j))) {
+					count++;
+				}
+				if (count > maxCount) {
+					maxCount = count;
+					maxUpperCaseWord = outArr[i];
+				}
+			}
+		}
+		return maxUpperCaseWord;
+	}
+
+	public static void main(String[] args) {
+		MaxUpperCaseWord obj = new MaxUpperCaseWord();
+
+		String inStr = "Good Mor9niNg FR3ienDs t2hi2s Is tEchnocredits";
+		System.out.println("Word with maximum uppercase characters is: " + obj.getWordWithMaxUpperCaseLetters(inStr));
+	}
+}
