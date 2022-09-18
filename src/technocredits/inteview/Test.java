fd5346fa13cf19dbc38756fa23c6dd3c981a7851
Krishna Kanani <kananikrishna427@gmail.com>
2022-09-18 13:47:14 +0530

    SEssion code

---
+++ src/technocredits/inteview/Test.java
@@ -0,0 +1,37 @@
+package technocredits.inteview;
+
+public class Test {
+
+	void m1(byte b) {
+		System.out.println(1);
+	}
+
+	void m1(short b) {
+		System.out.println(2);
+	}
+
+	/*
+	 * void m1(int b) { System.out.println(3); }
+	 */
+
+	/*
+	 * void m1(long l) { System.out.println(4); }
+	 */
+
+	/*
+	 * void m1(float b) { System.out.println(5); }
+	 */
+
+	/*
+	 * void m1(double b) { System.out.println(6); }
+	 */
+
+	void m1(String b) {
+		System.out.println(7);
+	}
+
+	void m1(Object b) {
+		System.out.println(8);
+	}
+
+}
