098358d303510ceb993b883326a7fdac141f8032
Smita Vetal <smita.bhoite27@gmail.com>
2022-09-19 22:00:16 +0530

    Assignment23_Smita

--- src/smitaVetal/ReturnPrimeNum.java
+++ src/smitaVetal/ReturnPrimeNum.java
@@ -1,33 +1,34 @@
 package smitaVetal;
 
 public class ReturnPrimeNum {
-	int count =0;
-	static int sum=0;
-	
+	int count = 0;
+	static int sum = 0;
+
 	boolean getPrimeNum(int num) {
 		boolean flag = true;
-		for (int index=2;index<=num/2;index++) {
-			if(num%index==0)
-				flag=false;
-			break;
+		for (int index = 2; index <= (num / 2); index++) {
+			if (num % index == 0)
+				flag = false;
 		}
-		if(flag) {
+			
+		
+		if (flag) {
 			count++;
-			sum=sum+num;
+			sum = sum + num;
 		}
-			return flag;
+		return true;
 	}
-	
+
 	int firstPrimeNum(int N) {
-		int num =2;
-		System.out.println("Sum of first "+ N+ " prime numbers are");
-		while(num >=2 && count<N) {
+		int num = 2;
+		System.out.println("Sum of first " + N + " prime numbers are");
+		while (num >= 2 && count < N) {
 			getPrimeNum(num);
-		num++;
+			num++;
 		}
 		return sum;
 	}
-	
+
 	public static void main(String[] args) {
 		new ReturnPrimeNum().firstPrimeNum(5);
 		System.out.println(sum);
