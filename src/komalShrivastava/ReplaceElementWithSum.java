9a3ba3b6a9b8d232b195809ea02b80f3fbacfb22
unknown <komald08@gmail.com>
2022-09-19 19:56:53 +0530

    Assignment24_Komal

---
+++ src/komalShrivastava/ReplaceElementWithSum.java
@@ -0,0 +1,26 @@
+package komalShrivastava;
+
+import java.util.Arrays;
+
+public class ReplaceElementWithSum {
+	
+	static int[] getReplacedArrayWithSumOfOtherElements(int[] input) {
+		int[] processedArray = new int[input.length];
+		for(int index=0; index<input.length; index++) {
+			int sum = 0;
+			for(int j=0; j<input.length; j++) {
+				if(index!=j) 
+					sum = sum + input[j];
+		    }
+		processedArray[index] = sum;
+		}
+		return processedArray;
+	}
+
+	public static void main(String[] args) {
+		int[] input = {12,2,11,17, 0, 1};
+		int []processedArray = getReplacedArrayWithSumOfOtherElements(input);
+		System.out.println("Input : " + Arrays.toString(input));
+		System.out.println("Output : " + Arrays.toString(processedArray));
+	}
+}
