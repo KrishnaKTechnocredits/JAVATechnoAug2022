3cdcb7b8492c3616535724d6b36ffffa525a2af9
Sagar Yadav <sgr.51291@gmail.com>
2022-09-20 16:50:17 +0530

    Assignment25_sagarY

---
+++ src/sagarY/Arrays/Assignment25_PrintRepeatedWords.java
@@ -0,0 +1,47 @@
+package sagarY.Arrays;
+import java.util.Arrays;
+
+public class Assignment25_PrintRepeatedWords {
+
+	void printRepeatWords(String str) {
+		String[] str1 = str.split(" ");
+		System.out.println("Given String array is: \n" + Arrays.deepToString(str1) + "\n");
+		System.out.print("Repeated words from th given array are:\n");
+
+		for (int i = 0; i < str1.length; i++) {
+			int count=0;
+			for (int j = i+1; j < str1.length; j++) {
+				if (str1[i].equals(str1[j])) {
+					count++;
+				}
+			}
+			if (count == 1 )
+				System.out.println(":" + str1[i]);
+		}
+	}
+
+	void printNonRepeatWords(String str) {
+		String[] str1 = str.split(" ");
+		System.out.print("\nNon Repeated words from th given array are:\n");
+
+		for (int i = 0; i < str1.length; i++) {
+			int count = 0;
+			for (int j = 1; j < str1.length; j++) {
+				if (str1[i].equals(str1[j]) && i != j) {
+					count++;
+				}
+			}
+			if (count == 0)
+				System.out.println(":" + str1[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+		String str = "Maulik Puja Hindavi Shubham Hindavi Shubham Titiksha Swati";
+		Assignment25_PrintRepeatedWords printRepeat = new Assignment25_PrintRepeatedWords();
+		printRepeat.printRepeatWords(str);
+		printRepeat.printNonRepeatWords(str);
+
+	}
+
+}
