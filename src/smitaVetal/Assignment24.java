0ec2664e5fa92f2195140b835e213991221a2ab5
Smita Vetal <smita.bhoite27@gmail.com>
2022-09-20 17:32:14 +0530

    Modified

--- src/smitaVetal/Assignment24.java
+++ src/smitaVetal/Assignment24.java
@@ -11,36 +11,33 @@ Hint : 12 should be replaced with - 2+11+17
           11 should be replaced with - 12+2+17
           17 should be replaced with - 12+2+11 */
 
-          
 package smitaVetal;
 
 import java.util.Arrays;
 
-
 public class Assignment24 {
-	
-	int[] getSumOfRestIntegers(int[] arr){
-		
+
+	int[] getSumOfRestIntegers(int[] arr) {
+
 		int[] output = new int[arr.length];
-		for(int i=0;i<arr.length;i++){
-			int sum=0;
-			for(int j=0;j<arr.length;j++){
-				if(i!=j){
-				sum = sum + arr[j];
+		for (int i = 0; i < arr.length; i++) {
+			int sum = 0;
+			for (int j = 0; j < arr.length; j++) {
+				if (i != j) {
+					sum = sum + arr[j];
 				}
 			}
 			output[i] = sum;
 		}
 		return output;
 	}
-	
-	public static void main(String[] args){
-	
-	Assignment24 array = new Assignment24();
-	int input[] = {12,2,11,17};
-	System.out.println("input array is :" + Arrays.toString(input));
-	int output[] = array.getSumOfRestIntegers(input);
-	System.out.println("Output array is : " + Arrays.toString(output));
+
+	public static void main(String[] args) {
+
+		Assignment24 array = new Assignment24();
+		int input[] = { 12, 2, 11, 17 };
+		System.out.println("input array is :" + Arrays.toString(input));
+		int output[] = array.getSumOfRestIntegers(input);
+		System.out.println("Output array is : " + Arrays.toString(output));
 	}
 }
-
