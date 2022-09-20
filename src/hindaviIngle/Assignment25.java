648c9de2fa91aab252a196da418d0f159ffb0dff
Hindavi16 <hindavijadhav@gmail.com>
2022-09-20 23:32:15 +0530

    modified code

--- src/hindaviIngle/Assignment25.java
+++ src/hindaviIngle/Assignment25.java
@@ -31,10 +31,11 @@ public class Assignment25 {
 	}
 		
 	void getNonRepeatedWord(String name) {
+		name=name.toUpperCase();
 		String[] input = name.split(" ");
 		for (int i = 0; i < input.length; i++) {
 			boolean flag = true;
-			for (int j = 1; j < input.length; j++) {
+			for (int j = 0; j < input.length; j++) {
 				if (input[i].equals(input[j]) && i != j)
 					flag = false;
 			}
