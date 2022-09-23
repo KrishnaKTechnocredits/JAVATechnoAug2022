0a68f59a1107a6b64fbaf52b8ef898a81a91b3ee
nilampatel26 <nilam192patel@gmail.com>
2022-09-23 17:45:47 +0530

    Assignment26_Nilam

---
+++ src/nilamP/StringOperations5.java
@@ -0,0 +1,25 @@
+package nilamP;
+
+import basic.assignment26.StringOperations;
+
+public class StringOperations5 {
+	void getReplaceChar(String input) {
+
+		while (input.length() > 0) {
+			char ch = input.charAt(0);
+			String str = Character.toString(ch);
+			int originalLength = input.length();
+			input = input.replace(str, "");
+			int newLength = input.length();
+			System.out.println(ch + " ->" + (originalLength - newLength));
+
+		}
+	}
+	public static void main(String[] args) {
+		StringOperations5 so = new StringOperations5();
+		String input = "technocredits";
+		System.out.println("input : " + input);
+		System.out.println("the frequency is: ");
+		so.getReplaceChar(input);
+	}
+}
