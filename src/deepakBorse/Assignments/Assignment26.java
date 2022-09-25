1ce983c4fbe07eb07c52ff9f577a2b5f248af619
Deepak Borse <deepak.borse27@gmail.com>
2022-09-25 17:23:26 +0530

    Assignment26_DeepakB

--- src/deepakBorse/Assignments/Assignment26.java
+++ src/deepakBorse/Assignments/Assignment26.java
@@ -7,32 +7,31 @@ String str = "technocreditst";
 
 package deepakBorse.Assignments;
 
+class Assignment26 {
 
-class Assignment26{
+	void getFreqchar(String input) {
+		int origstrlen = 0, newstrLen = 0, freqcount = 0;
 
-	void getFreqchar(String input){
-		int origstrlen =0,newstrLen=0,freqcount=0;
-		
-		while(input.length()>0) {
-			char ch=input.charAt(0);
-			origstrlen=input.length();// get the original string length
-			String str=Character.toString(ch);// Convert character to string
-			input=input.replace(str,"");// Replace ch with blank
-			newstrLen=input.length(); //get the new length of the string
-			
-			freqcount=origstrlen-newstrLen;//
-			
-			System.out.println(ch+" -> "+freqcount);
+		while (input.length() > 0) {
+			char ch = input.charAt(0);
+			origstrlen = input.length();// get the original string length
+			String str = Character.toString(ch);// Convert character to string
+			input = input.replace(str, "");// Replace ch with blank
+			newstrLen = input.length(); // get the new length of the string
+
+			freqcount = origstrlen - newstrLen;// Calculate the frequency of the character
+
+			System.out.println(ch + " -> " + freqcount);
 		}
 	}
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
 		Assignment26 Assignment = new Assignment26();
-		
-		String input=("technocreditst");
-		System.out.println("Frequency of each character in given string '"+input+"' is as follows");
+
+		String input = ("technocreditst");
+		System.out.println("Frequency of each character in given string '" + input + "' is as follows");
 		Assignment.getFreqchar(input);
-	
+
 	}
-}	
+}
