355bbd77748185a482c3ad2a6c336b7155365c9e
Hindavi16 <hindavijadhav@gmail.com>
2022-09-28 21:09:21 +0530

    Problem stmt added

--- src/hindaviIngle/Assignment22.java
+++ src/hindaviIngle/Assignment22.java
@@ -1,3 +1,12 @@
+
+/*
+ * Assignment - 22 : 15th Sep'2022
+
+Replace current index number with 0, if index+1 number is smaller then index number.
+int[] arr = {10,20,30,22,44,21,92,89};
+output : {10,20,0,22,0,21,0,89}
+
+ */
 package hindaviIngle;
 
 import java.util.Arrays;
