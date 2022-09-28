d72b4d986f003b02b68cf10a13bfcde6b32d8069
Asthasrivas <astha.srivas1992@gmail.com>
2022-09-28 23:43:35 +0530

    Assignment30_Astha

--- src/asthaSrivastava/assignments/DuplicateNumbers28Sept.java
+++ src/asthaSrivastava/assignments/DuplicateNumbers28Sept.java
@@ -1,3 +1,17 @@
+/*Assignment - 30 : 28th Sep'2022
+
+Find duplicate numbers from given array.
+input : {10,12,44,12,77,67,12,44};
+output : 12
+         44 
+
+Hint : 2 for loop, think about replacement stretagy (the number you compare, replace that number with other number)
+
+
+if(arr[i] == arr[j] && i!=j){
+	arr[j] = 0;
+}
+*/
 package asthaSrivastava.assignments;
 
 public class DuplicateNumbers28Sept {
