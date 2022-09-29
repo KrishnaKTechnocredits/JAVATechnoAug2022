575a9efaa6d665d0d042e6eb74d84f83b669e677
Asthasrivas <astha.srivas1992@gmail.com>
2022-09-30 02:04:02 +0530

    Assignment31_Astha

---
+++ src/asthaSrivastava/string/Pallindrom.java
@@ -0,0 +1,76 @@
+/*Assignment - 31 : 29th Sep'2022
+
+Program 1 :
+Write a method to find whether given String is pallindrome.
+
+input : radar
+output : radar is pallindrom
+
+input : aabcbaa
+output : aabcbaa is pallindrom
+
+input : abcabc
+output : abcabc is not a pallindrom
+
+
+Program 2 :
+Write a method to print all pallindrome input from given array.
+
+input : {"radar","techhpet","madam","maulikkilum"};
+output : radar
+         madam*/
+
+package asthaSrivastava.string;
+
+import java.util.Arrays;
+
+public class Pallindrom {
+
+	// method to find Pallindrom
+	boolean isPallindromString(String str) {
+		boolean flag = true;
+
+		for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
+			if (str.charAt(i) != str.charAt(j)) {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+
+	// method to print Pallindrom
+	void displayPallindrome(String input) {
+
+		if (isPallindromString(input)) {
+			System.out.println(input + " is pallindrome");
+		} else {
+			System.out.println(input + " is not pallindrome");
+		}
+	}
+	//method to find all Pallindrom from given array of String
+	void printAllPallindrome(String[] arr) {
+		for (int index = 0; index < arr.length; index++) {
+			String name = arr[index];
+			boolean flag = isPallindromString(name);
+
+			if (flag) {
+				System.out.println(name + " is pallindrome");
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Pallindrom pm = new Pallindrom();
+		pm.displayPallindrome("radar");
+		pm.displayPallindrome("aabcbaa");
+		pm.displayPallindrome("abcabc");
+		System.out.println("--------------------------");
+
+		String[] input = { "radar", "techhpet", "madam", "maulikkilum" };
+		
+		pm.printAllPallindrome(input);
+
+	}
+
+}
