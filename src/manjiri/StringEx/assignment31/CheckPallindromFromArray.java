aa5591a05e5cb658416fb2c0fbf54ec3830b3ca2
ManjiriC0406 <manjiri.chourikar@gmail.com>
2022-09-29 18:37:35 +0530

    Assignment31-Manjiri

---
+++ src/manjiri/StringEx/assignment31/CheckPallindromFromArray.java
@@ -0,0 +1,31 @@
+/*Write a method to print all pallindrome input from given array.
+
+input : {"radar","techhpet","madam","maulikkilum"};
+output : radar
+         madam*/
+
+package manjiri.StringEx.assignment31;
+
+import java.util.Arrays;
+
+import manjiri.StringEx.assignment31.CheckPallindromWithoutReversingString;
+
+public class CheckPallindromFromArray {
+	
+	void getPallindromString(String[] input){
+		
+		System.out.println("Input Array is: " + Arrays.toString(input));
+		System.out.println("-------------------------------------------------------");
+		
+		CheckPallindromWithoutReversingString checkPallindrome = new CheckPallindromWithoutReversingString();
+		for (int index = 0; index < input.length; index++) {
+			checkPallindrome.CheckPallindromWithoutStringReverse(input[index]);
+		}	
+	}
+	
+	public static void main(String[] args) {
+		String[] inputArr = {"radar","techhpet","madam","maulikkilum"};
+		new CheckPallindromFromArray().getPallindromString(inputArr);
+	}
+
+}
