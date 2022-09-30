9e127eecbcab29575a21fb6db1b2f541fbf3994b
ShivaniLikhia <shivanilikhiya@gmail.com>
2022-09-30 22:18:31 +0530

    Assignment31_ShivaniL added third program

--- src/shivaniLikhia/Assignment32.java
+++ src/shivaniLikhia/Assignment32.java
@@ -13,6 +13,15 @@ Program 2 :
 Write a method to return reverse number.
 input : 123
 output : 321
+
+Program 3 : 
+write a method to verify number is palindrome.
+
+input : 1221
+output : 1221 is a palindrome number
+
+input : 1231
+output : 1231 is not a palindrome number
 */
 
 package shivaniLikhia;
@@ -36,7 +45,7 @@ public class Assignment32 {
 	}
 	
 	void getReverseNum(int num) {
-		System.out.println("The reverse number is : ");
+		System.out.println("The reverse number for the given number "+num+" is : ");
 		while(num>0) {
 			int r = num % 10;
 			System.out.print(r);
@@ -44,11 +53,32 @@ public class Assignment32 {
 		}
 	}
 	
+	void verifyNumberPallindrome(int num) {
+		String originalNum = Integer.toString(num);
+		String ans = "";
+		while(num>0) {
+			int r = num % 10;
+			ans = ans + Integer.toString(r);
+			num = num/10;
+		}
+		if(ans.equals(originalNum)) {
+			System.out.println("The given number "+ originalNum +" is a Pallindrome Number.");
+		}
+		else {
+			System.out.println("The given number "+ originalNum +" is not a Pallindrome Number.");
+		}
+	}
+	
 	public static void main(String[] args) {
 		Assignment32 assignment32 = new Assignment32();
 		assignment32.verifyNumberIsArmStrong(153);
 		assignment32.verifyNumberIsArmStrong(123);
-		System.out.println("----------------------------------------------");
+		System.out.println("------------------------------------------------------");
 		assignment32.getReverseNum(123);
+		System.out.println(" ");
+		System.out.println("------------------------------------------------------");
+		assignment32.verifyNumberPallindrome(1221);
+		assignment32.verifyNumberPallindrome(1231);
+		
 	}
 }
