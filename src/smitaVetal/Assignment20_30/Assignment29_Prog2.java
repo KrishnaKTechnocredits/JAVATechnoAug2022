85756d372f5906fcca14beb642253a75d62554b1
Smita Vetal <smita.bhoite27@gmail.com>
2022-09-30 12:07:56 +0530

    Modified

--- src/smitaVetal/Assignment20_30/Assignment29_Prog2.java
+++ src/smitaVetal/Assignment20_30/Assignment29_Prog2.java
@@ -10,31 +10,31 @@ package smitaVetal.Assignment20_30;
 
 import java.util.Arrays;
 
-public class Assignment29_Prog2 {
+class Assignment29_Prog2 {
 
 	int[] getNum(int[] input) {
 		int[] output = new int[input.length];
 		int count = 0;
-		for(int index=0;index<input.length;index++) {
-			if(input[index]==0) {
+		for (int index = 0; index < input.length; index++) {
+			if (input[index] == 0) {
 				count++;
 			}
 		}
-		for (int innerIndex = 0;innerIndex<input.length;innerIndex++) {
-			if(input[innerIndex]!=0) {
-				output[count]=input[innerIndex];
+		for (int innerIndex = 0; innerIndex < input.length; innerIndex++) {
+			if (input[innerIndex] != 0) {
+				output[count] = input[innerIndex];
 				count++;
 			}
 		}
 		return output;
 	}
+
 	public static void main(String[] args) {
 		Assignment29_Prog2 prog = new Assignment29_Prog2();
-		int[] input = {10,0,0,3,4,5,0,9};
-		System.out.println("Input array is : "+Arrays.toString(input));
+		int[] input = { 10, 0, 0, 3, 4, 5, 0, 9 };
+		System.out.println("Input array is : " + Arrays.toString(input));
 		int[] output = prog.getNum(input);
-		System.out.println("Output after shifting zeros at start is : " +Arrays.toString(output));
+		System.out.println("Output after shifting zeros at start is : " + Arrays.toString(output));
 	}
-	
 
 }
