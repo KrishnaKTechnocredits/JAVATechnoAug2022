b51c561096adaa5638f6b88e8c566cd41feb4832
PriyankaDhobale <dhobale.priyanka07@gmail.com>
2022-09-30 11:31:33 +0530

    Assignment_25_to_27_priyanka

---
+++ src/priyankaD/assignments_20_30/Assignment_27.java
@@ -0,0 +1,51 @@
+package priyankaD.assignments_20_30;
+import java.util.Arrays;
+public class Assignment_27 {
+	/*Assignment-27 
+	  1. Write a program to print unique character of each name in given string array.
+	 String[] arr = {"Maulik", "Kanani","Mahesh", "Kanani"};*/
+	String printUniqueCharOfName(String name) {
+		for (int index=0;index<name.length();index++) {
+			char ch=name.charAt(index);
+			if(name.indexOf(ch)==name.lastIndexOf(ch)) {
+				System.out.println(ch);
+			}
+		}System.out.println("-------------------");
+		return name;
+	}
+	void getStringOfName(String[] arr) {
+		for(int index=0;index<arr.length;index++) {
+			System.out.println("Name is -->"+arr[index]);
+			String name=printUniqueCharOfName(arr[index]);
+		}
+	}
+	/*2.  Write a program to print 1st unique character of each name in given string array.
+	 String[] arr = {"Maulik", "KananiK", "akanksha"};*/
+
+	String printFirstUniqueCharacterOfName(String name) {
+		for (int index=0;index<name.length();index++) {
+			char ch=name.charAt(index);
+			if(name.indexOf(ch)==name.lastIndexOf(ch)) {
+				System.out.println(ch);
+				break;
+			}
+		}System.out.println("-------------------");
+		return name;
+	}
+	void getArrayStringOfName(String[] arr1) {
+		for(int index=0;index<arr1.length;index++) {
+			System.out.println("Name is -->"+arr1[index]);
+			String name=printFirstUniqueCharacterOfName(arr1[index]);
+		}
+	}
+	public static void main(String[]args) {
+		Assignment_27 assignment=new Assignment_27();
+		String[] arr={"Maulik", "Kanani","Mahesh", "Kanani"};
+		System.out.println(Arrays.toString(arr));
+		assignment.getStringOfName(arr);
+		System.out.println("----*-----*-----*-----*-----*-----");
+		String[] arr1={"Maulik", "KananiK", "akanksha"};
+		System.out.println(Arrays.toString(arr1));
+		assignment.getArrayStringOfName(arr1);
+	}
+}
