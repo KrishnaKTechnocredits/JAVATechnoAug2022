4633b4537cb396b1a4c5c8c2174dfc3477fc610b
Sagar Yadav <sgr.51291@gmail.com>
2022-10-04 19:42:56 +0530

    Factorial program modified

--- src/sagarY/Assignment_34/FindFactorial.java
+++ src/sagarY/Assignment_34/FindFactorial.java
@@ -1,3 +1,7 @@
+/*Program 2 : Write a method to return factorial of user defined given number (using scanner class).
+input : 5
+outut : 120*/
+
 package sagarY.Assignment_34;
 import java.util.Scanner;
 
@@ -5,23 +9,27 @@ public class FindFactorial {
 	int factorial=1;
 	
 	void findFactorial(int num) {
-
-		while(num>0) {
-			System.out.print(num+"*");
-			factorial = factorial*num;
-			num--;
+		int num1=1;
+		while(num1<=num) {
+			System.out.print(num1);
+			factorial = factorial*num1;
+			num1++;
+			if(num1<=5)
+				System.out.print("*");
 		}
 		
 		System.out.println("=" +factorial);
 	}
 	
 	public static void main(String[] args) {
+		System.out.println("Program 2 : Write a method to return \nfactorial of user defined given number");
+		System.out.println("------------------------------------------");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number to check its factorial: ");
+		System.out.print("Enter number to check its factorial: ");
 		int num=sc.nextInt();
 		FindFactorial fact =  new FindFactorial();
 		System.out.println("=====================================");
-		System.out.println("Output:");
+		System.out.println("Output: Factorial of "+num+" is");
 		fact.findFactorial(num);
 		sc.close();
 	}
