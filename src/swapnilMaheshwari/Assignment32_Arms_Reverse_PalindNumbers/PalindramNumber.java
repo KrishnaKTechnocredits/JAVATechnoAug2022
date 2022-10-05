2cb9617bb889db66c6bd52234fa0ad1ecefd3b21
Swapnil Maheshwari <swapnilmaheshwari5@outlook.com>
2022-10-06 01:05:38 +0530

    Indentation updated

--- src/swapnilMaheshwari/Assignment32_Arms_Reverse_PalindNumbers/PalindramNumber.java
+++ src/swapnilMaheshwari/Assignment32_Arms_Reverse_PalindNumbers/PalindramNumber.java
@@ -15,24 +15,25 @@ output : 1231 is not a palindrome number
 import java.util.Scanner;
 
 public class PalindramNumber {
-	void getPalindramNumber (int num) {
-		int r=0;
-		int reverseNum=0;
-		int OrginalNum=num;
-		while (num>0) {
-			r=num%10;
-			reverseNum=reverseNum*10+r;
-			num=num/10;
+	void getPalindramNumber(int num) {
+		int r = 0;
+		int reverseNum = 0;
+		int OrginalNum = num;
+		while (num > 0) {
+			r = num % 10;
+			reverseNum = reverseNum * 10 + r;
+			num = num / 10;
 		}
-		if (reverseNum==OrginalNum)
-			System.out.println("Number is Palindram :"+OrginalNum);
+		if (reverseNum == OrginalNum)
+			System.out.println("Number is Palindram :" + OrginalNum);
 		else
-			System.out.println("Number is not a Palindram:"+OrginalNum);
+			System.out.println("Number is not a Palindram:" + OrginalNum);
 	}
+
 	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the Number");
-		int num=scanner.nextInt();
+		int num = scanner.nextInt();
 		new PalindramNumber().getPalindramNumber(num);
 	}
 }
--- src/swapnilMaheshwari/Assignment32_Arms_Reverse_PalindNumbers/ReverseNumber.java
+++ src/swapnilMaheshwari/Assignment32_Arms_Reverse_PalindNumbers/ReverseNumber.java
@@ -11,20 +11,21 @@ package swapnilMaheshwari.Assignment32_Arms_Reverse_PalindNumbers;
 import java.util.Scanner;
 
 public class ReverseNumber {
-	int getReverseNumber (int num) {
-		int r=0;
-		int reverseNum=0;
-		while (num>0) {
-			r=num%10;
-			reverseNum=reverseNum*10+r;
-			num=num/10;
+	int getReverseNumber(int num) {
+		int r = 0;
+		int reverseNum = 0;
+		while (num > 0) {
+			r = num % 10;
+			reverseNum = reverseNum * 10 + r;
+			num = num / 10;
 		}
 		return reverseNum;
 	}
+
 	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the Number");
-		System.out.println ("The reverser number is :"+new ReverseNumber().getReverseNumber(scanner.nextInt()));
+		System.out.println("The reverser number is :" + new ReverseNumber().getReverseNumber(scanner.nextInt()));
 	}
 
 }
