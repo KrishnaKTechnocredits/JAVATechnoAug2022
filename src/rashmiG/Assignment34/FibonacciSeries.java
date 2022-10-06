08a920b0eca518ef6824535564059628c67366bb
Rashmi Ganachari <rashmiiganachari282@gmail.com>
2022-10-07 02:38:05 +0530

    Assignment34_RashmiG_handled negative scenarios

--- src/rashmiG/Assignment34/FibonacciSeries.java
+++ src/rashmiG/Assignment34/FibonacciSeries.java
@@ -11,17 +11,20 @@ import java.util.Scanner;
 public class FibonacciSeries {
 
 	void displayFibonacciSeries(int size) {
-		int num1 = 0;
-		int num2 = 1;
-		System.out.print(num1 + " ");
-		System.out.print(num2 + " ");
-		for (int index = 1; index <= size - 2; index++) {
-			int temp = num2;
-			num2 = num1 + num2;
-			num1 = temp;
-
+		if (size > 0) {
+			int num1 = 0;
+			int num2 = 1;
+			System.out.print(num1 + " ");
 			System.out.print(num2 + " ");
-		}
+			for (int index = 1; index <= size - 2; index++) {
+				int temp = num2;
+				num2 = num1 + num2;
+				num1 = temp;
+
+				System.out.print(num2 + " ");
+			}
+		} else
+			System.out.println("Invalid input");
 	}
 
 	public static void main(String[] args) {
