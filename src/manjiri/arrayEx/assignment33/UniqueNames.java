f4ce178d643007559140d84ea955e7d5769a2ca3
ManjiriC0406 <manjiri.chourikar@gmail.com>
2022-10-06 20:53:22 +0530

    Assignmnet33-Manjiri Updated

--- src/manjiri/arrayEx/assignment33/UniqueNames.java
+++ src/manjiri/arrayEx/assignment33/UniqueNames.java
@@ -11,37 +11,36 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class UniqueNames {
-	
-	void printUniqueNames(String[] input){
-		
+
+	void printUniqueNames(String[] input) {
 		for (int index = 0; index < input.length; index++) {
-			boolean flag = true;
-			//compare outer index with each inner index and check if match found
-			for (int innerIndex = 0; innerIndex < input.length; innerIndex++) {
-				if(input[index].equalsIgnoreCase(input[innerIndex])&& index != innerIndex) {
-					flag = false;
+			// compare outer index with each inner index and check if match found
+			if (!input[index].equals("")) {
+				for (int innerIndex = 0; innerIndex < input.length; innerIndex++) {
+					if (input[index].equalsIgnoreCase(input[innerIndex]) && index != innerIndex) {
+						input[innerIndex] = "";
+					}
 				}
-			}
-			if(flag) {
-				System.out.println(input[index]);
+				if (input[index] != "")
+					System.out.println(input[index]);
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("How many elements you want to enter: ");
 		int length = sc.nextInt();
-		
+
 		String[] inputArr = new String[length];
 		for (int index = 0; index < inputArr.length; index++) {
 			inputArr[index] = sc.next();
 		}
 		sc.close();
-		
+
 		System.out.println(Arrays.toString(inputArr));
 		System.out.println("=========================================================");
-		
+
 		new UniqueNames().printUniqueNames(inputArr);
 	}
 }
