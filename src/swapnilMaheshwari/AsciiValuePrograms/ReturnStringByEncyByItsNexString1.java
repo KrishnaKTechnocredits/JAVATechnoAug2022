f9d2d61fc30fed2c591c0808c3c11578d77c0c3d
Swapnil Maheshwari <swapnilmaheshwari5@outlook.com>
2022-10-11 01:06:13 +0530

    Assignment36_Swapnil_Updataed_Code

--- src/swapnilMaheshwari/AsciiValuePrograms/ReturnStringByEncyByItsNexString1.java
+++
@@ -1,5 +0,0 @@
-package swapnilMaheshwari.AsciiValuePrograms;
-
-public class ReturnStringByEncyByItsNexString1 {
-
-}
--- src/swapnilMaheshwari/AsciiValuePrograms/ReturnStringByEncyByItsNextString.java
+++ src/swapnilMaheshwari/AsciiValuePrograms/ReturnStringByEncyByItsNextString.java
@@ -1,21 +1,9 @@
 /*
- Program 7 : 
+ Program 1 : 
 WAP to return String by encrypting all characters from given string by its next character.
 
-input : aPd%3zZ
-output : bQe%3aA
-
-if(ch =='Z'){
-	output = output + 'A';
-}else if(ch =='z'){
-	output = output + 'a';
-}else{
-	if((ch>=65 && ch<=90) || (ch>=97 && ch<=122)){
-		ch++;
-	}
-	output = output + ch;
-}
-
+input : apdz
+output : bqea
 
  */
 
@@ -26,29 +14,25 @@ import java.util.Scanner;
 public class ReturnStringByEncyByItsNextString {
 
 	String getReturnOfString(String str) {
-		String output = "";
-		for (int index = 0; index < str.length(); index++) {
-			char ch = str.charAt(index);
-			if (ch == 'z') {
-				output += 'a';
-			} else if (ch == 'Z')
-				output += 'A';
-			else if (ch >= 65 && ch <= 90 || ch >= 97 && ch <= 122)
-				output = output + (char) (ch + 1);
+		String output="";
+		for (int index=0; index<str.length(); index++) {
+			char ch=str.charAt(index);
+			if (ch=='z') {
+				output+='a';
+			}
 			else
-				output = output + ch;
+				output=output+(char)(ch+1);
 		}
 		return output;
 	}
-
 	public static void main(String[] args) {
-		Scanner src = new Scanner(System.in);
+		Scanner src=new Scanner(System.in);
 		System.out.println("Enter the String");
-		String str = src.next();
-		System.out.print("input :" + str);
+		String str=src.next();
+		System.out.print("input :" +str);
 		String strReturn;
-		strReturn = new ReturnStringByEncyByItsNextString().getReturnOfString(str);
+		strReturn=new ReturnStringByEncyByItsNextString().getReturnOfString(str);
 		System.out.println();
-		System.out.println("Output :" + strReturn);
+		System.out.println("Output :"+strReturn);
 	}
 }
