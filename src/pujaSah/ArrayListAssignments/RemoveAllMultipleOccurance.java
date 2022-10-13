8a6c1789cea3e83ee8fbc8c4e1e23d4e62cfa3bb
Puja Sah <pujasah5@gmail.com>
2022-10-13 09:17:45 +0530

    adding optimized method

--- src/pujaSah/ArrayListAssignments/RemoveAllMultipleOccurance.java
+++ src/pujaSah/ArrayListAssignments/RemoveAllMultipleOccurance.java
@@ -15,8 +15,21 @@ import java.util.ArrayList;
 
 public class RemoveAllMultipleOccurance {
 
-	// method to remove multiple occurance from ArrayList.
+	// method to remove multiple occurrence from ArrayList.
 	void removeAllMultipleOccuranceOfNamesFromList(ArrayList<String> listOfName) {
+		System.out.println("Original List: " + "\n" + listOfName + "\n");
+		for (int index = 0; index < listOfName.size(); index++) {
+			String name = listOfName.get(index);
+			if(listOfName.indexOf(name) != listOfName.lastIndexOf(name)) {
+				listOfName.remove(listOfName.lastIndexOf(name));
+				index--;
+			}
+		}
+		System.out.println("Processed list after removing all the multiple occurance of names is: " + "\n" + listOfName);
+	}
+	
+	// not an optimized method
+	void removeAllMultipleOccuranceOfNamesFromList1(ArrayList<String> listOfName) {
 		System.out.println("Original List: " + "\n" + listOfName + "\n");
 		for (int index = 0; index < listOfName.size(); index++) {
 			String name = listOfName.get(index);
@@ -30,15 +43,16 @@ public class RemoveAllMultipleOccurance {
 	public static void main(String[] args) {
 		ArrayList<String> listOfName = new ArrayList<String>();
 		listOfName.add("Sagar");
-		listOfName.add("Harshal");
 		listOfName.add("Sagar");
+		listOfName.add("Sagar");
+		listOfName.add("Harshal");
+		listOfName.add("Harshal");
 		listOfName.add("Apurva");
 		listOfName.add("Gaurav");
-		listOfName.add("Sagar");
-		listOfName.add("Deepak");
-		listOfName.add("Rashmi");
-		listOfName.add("Sagar");
-
+		listOfName.add("Gaurav");
+		listOfName.add("Apurva");
+		listOfName.add("Apurva");
+		
 		new RemoveAllMultipleOccurance().removeAllMultipleOccuranceOfNamesFromList(listOfName);
 	}
 }
