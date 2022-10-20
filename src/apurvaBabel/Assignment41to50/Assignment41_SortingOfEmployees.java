00cd5b015e6ab01a70211bd0af2cc947a831906c
Apurva Babel <babelapurva96@gmail.com>
2022-10-20 20:04:26 +0530

    Assignment41_ApurvaB

---
+++ src/apurvaBabel/Assignment41to50/Assignment41_SortingOfEmployees.java
@@ -0,0 +1,29 @@
+package apurvaBabel.Assignment41to50;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Assignment41_SortingOfEmployees {
+
+	public static void main(String[] args) {
+		ArrayList<Employee> listOfEmployees = new ArrayList<Employee>();
+
+		listOfEmployees.add(new Employee("Apurva", 1, 5000, 1001));
+		listOfEmployees.add(new Employee("Sejal", 8, 4000, 1002));
+		listOfEmployees.add(new Employee("Priti", 3, 4000, 1003));
+		listOfEmployees.add(new Employee("Ashish", 5, 6000, 1005));
+		listOfEmployees.add(new Employee("Vaibhav", 2, 3500, 1005));
+		listOfEmployees.add(new Employee("Apurva", 7, 5000, 1001));
+		listOfEmployees.add(new Employee("Nikhli", 4, 6500, 1004));
+		listOfEmployees.add(new Employee("Sanjay", 9, 5500, 1002));
+		listOfEmployees.add(new Employee("Vaishnavi", 6, 3500, 1002));
+		listOfEmployees.add(new Employee("Apurva", 10, 2000, 1003));
+
+		Collections.sort(listOfEmployees);
+		System.out.println("Employee Details -> EmpId, EmpName, EmpSalary, EmpDeptId\n");
+		for (Employee list : listOfEmployees) {
+			System.out.println(list.getEmpId() + " -> " + list.getEmpName() + " -> " + list.getEmpSalary() + " -> "
+					+ list.getEmpDeptId());
+		}
+	}
+}
---
+++ src/apurvaBabel/Assignment41to50/Employee.java
@@ -0,0 +1,68 @@
+/*Assignment - 41 :
+Sort Employee object based on Salary.
+Employee class should have empName, empId, empSalary, empDeptId as parameters.
+Print all instance variable of employee after sorting.
+*/
+package apurvaBabel.Assignment41to50;
+
+public class Employee implements Comparable<Employee> { // Pojo class of emloyee
+
+	private String empName;
+	private int empId;
+	private int empSalary;
+	private int empDeptId;
+
+	public Employee(String empName, int empId, int empSalary, int empDeptId) {
+		super();
+		this.empName = empName;
+		this.empId = empId;
+		this.empSalary = empSalary;
+		this.empDeptId = empDeptId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public int getEmpSalary() {
+		return empSalary;
+	}
+
+	public void setEmpSalary(int empSalary) {
+		this.empSalary = empSalary;
+	}
+
+	public int getEmpDeptId() {
+		return empDeptId;
+	}
+
+	public void setEmpDeptId(int empDeptId) {
+		this.empDeptId = empDeptId;
+	}
+
+	// Overridden method from comparable interface 
+	@Override
+	public int compareTo(Employee o) {
+		if (this.empSalary != o.empSalary) {
+			return this.empSalary - o.empSalary;
+		} else if (!this.empName.equals(o.empName) ) {
+			return this.empName.compareTo(o.empName);
+		} else if (this.empDeptId != o.empDeptId) {
+			return this.empDeptId - o.empDeptId;
+		} else {
+			return this.empId - o.empId;
+		}
+	}
+}
