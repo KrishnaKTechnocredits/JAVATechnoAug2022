e14b391287578fbb131fe074671d92469fc62cdb
ManjiriC0406 <manjiri.chourikar@gmail.com>
2022-10-20 22:59:14 +0530

    Assignment41-Manjiri

---
+++ src/manjiri/assignment41/Employee.java
@@ -0,0 +1,43 @@
+/*Sort Employee object based on Salary.
+Employee class should have empName, empId, empSalary, empDeptId as parameters.
+Print all instance variable of employee after sorting.
+*/
+
+//POJO class
+package manjiri.assignment41;
+
+public class Employee implements Comparable<Employee> {
+	String empName;
+	int empid;
+	double empSalary;
+	int empDeptId;
+
+	public Employee(String empName, int empid, double empSalary, int empDeptId) {
+		super();
+		this.empName = empName;
+		this.empid = empid;
+		this.empSalary = empSalary;
+		this.empDeptId = empDeptId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public int getEmpid() {
+		return empid;
+	}
+
+	public double getEmpSalary() {
+		return empSalary;
+	}
+
+	public int getEmpDeptId() {
+		return empDeptId;
+	}
+
+	@Override
+	public int compareTo(Employee o) {
+		return (int) (this.empSalary - o.empSalary);
+	}
+}
