fe70d3295cc6246896f7ddb18bdce87520a11b60
Apurva Babel <babelapurva96@gmail.com>
2022-10-21 13:58:28 +0530

    Assignment42_ApurvaB

---
+++ src/apurvaBabel/Assignment42/EmployeeIdComparator.java
@@ -0,0 +1,11 @@
+package apurvaBabel.Assignment42;
+
+import java.util.Comparator;
+
+public class EmployeeIdComparator implements Comparator<Employee> {
+
+	@Override
+	public int compare(Employee o1, Employee o2) {
+		return o1.getEmpId() - o2.getEmpId();
+	}
+}
