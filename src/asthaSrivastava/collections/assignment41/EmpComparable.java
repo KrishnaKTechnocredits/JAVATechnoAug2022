07c7a529f4557ec5696f4e8a5b793b38e5432f1c
Asthasrivas <astha.srivas1992@gmail.com>
2022-10-22 01:35:03 +0530

    Assignment41_Astha

---
+++ src/asthaSrivastava/collections/assignment41/EmpComparable.java
@@ -0,0 +1,21 @@
+package asthaSrivastava.collections.assignment41;
+
+import java.util.*;
+
+public class EmpComparable {
+	
+	
+
+	public static void main(String[] args) {
+		TreeSet<Employee> emp1 = new TreeSet<Employee>();
+		emp1.add(new Employee("Karuna", 123, 50000, 1));
+		emp1.add(new Employee("KK", 124, 55500, 1));
+		emp1.add(new Employee("Purav", 125, 60000, 2));
+		emp1.add(new Employee("Tanmay", 126, 90000, 3));
+		
+		for(Employee emp : emp1) {
+			System.out.println(emp.getEmpName() +"-"+ emp.getEmpId() +"-"+ emp.getEmpSalary() +"-"+ emp.getEmpDeptId() );
+			
+		}
+	}
+}
---
+++ src/asthaSrivastava/collections/assignment41/Employee.java
@@ -0,0 +1,42 @@
+/*Sort Employee object based on Salary.
+Employee class should have empName, empId, empSalary, empDeptId as parameters.
+Print all instance variable of employee after sorting.*/
+
+package asthaSrivastava.collections.assignment41;
+
+public class Employee implements Comparable<Employee> {
+	public Employee(String empName, int empId, int empSalary, int empDeptId) {
+		super();
+		this.empName = empName;
+		this.empId = empId;
+		this.empSalary = empSalary;
+		this.empDeptId = empDeptId;
+
+	}
+
+	private String empName;
+	private int empId, empSalary,empDeptId;
+	
+	public String getEmpName() {
+		return empName;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public int getEmpSalary() {
+		return empSalary;
+	}
+
+	public int getEmpDeptId() {
+		return empDeptId;
+	}
+
+	@Override
+	public int compareTo(Employee o) {
+		// TODO Auto-generated method stub
+		return (int) (this.empSalary - o.empSalary);
+	}
+
+}
