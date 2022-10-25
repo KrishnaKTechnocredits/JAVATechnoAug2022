1ea82eb65deb6e518940490f94bc64e2f68603b7
Asthasrivas <astha.srivas1992@gmail.com>
2022-10-25 19:22:05 +0530

    Assignment42_Astha

--- src/asthaSrivastava/assignment42/EmpDetaisCompare.java
+++ src/asthaSrivastava/assignment42/EmpDetaisCompare.java
@@ -6,22 +6,23 @@ public class EmpDetaisCompare {
 
 	ArrayList<Employee> getEmployeeCompDetails() {
 		ArrayList<Employee> listOfEmp = new ArrayList<Employee>();
-		listOfEmp.add(new Employee("Priyanka",367724,50, 56009.4));
-		listOfEmp.add(new Employee("Vimal",367725,53, 98000));
-		listOfEmp.add(new Employee("Sai",367799,53, 88000));
-		listOfEmp.add(new Employee("Amaresh",244455,54, 75000));
-		
+		listOfEmp.add(new Employee("Priyanka", 367724, 50, 56009.4));
+		listOfEmp.add(new Employee("Vimal", 367725, 53, 98000));
+		listOfEmp.add(new Employee("Sai", 367799, 53, 88000));
+		listOfEmp.add(new Employee("Amaresh", 244455, 54, 75000));
+
 		System.out.println("Sorting based om Salary");
 		Collections.sort(listOfEmp, new EmployeeSalaryCompare());
 		return listOfEmp;
-
 	}
+
 	public static void main(String[] args) {
 		EmpDetaisCompare emp1 = new EmpDetaisCompare();
-		//System.out.println("Sorted Sequence is");
+		// System.out.println("Sorted Sequence is");
 		ArrayList<Employee> listOfEmp = emp1.getEmployeeCompDetails();
-		for(Employee emp : listOfEmp) {
-			System.out.println(emp.getEmpName() +"-->"+ emp.getEmpDeptId() +"-->"+ emp.getEmpId() +"-->"+ emp.getEmpSalary());
-	}
+		for (Employee emp : listOfEmp) {
+			System.out.println(emp.getEmpName() + "-->" + emp.getEmpDeptId() + "-->" + emp.getEmpId() + "-->"
+					+ emp.getEmpSalary());
+		}
 	}
 }
--- src/asthaSrivastava/assignment42/EmployeeSalaryCompare.java
+++ src/asthaSrivastava/assignment42/EmployeeSalaryCompare.java
@@ -2,12 +2,12 @@ package asthaSrivastava.assignment42;
 
 import java.util.Comparator;
 
-public class EmployeeSalaryCompare implements Comparator <Employee> {
+public class EmployeeSalaryCompare implements Comparator<Employee> {
 
 	@Override
 	public int compare(Employee o1, Employee o2) {
-		Employee emp1 = (Employee)o1;
-		Employee emp2 =(Employee) o2;
-		return (int) (emp1.getEmpSalary()- (emp2.getEmpSalary()));
+		Employee emp1 = (Employee) o1;
+		Employee emp2 = (Employee) o2;
+		return (int) (emp1.getEmpSalary() - (emp2.getEmpSalary()));
 	}
 }
