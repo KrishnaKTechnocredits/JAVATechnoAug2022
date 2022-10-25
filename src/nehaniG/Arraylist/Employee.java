458f4c3718146cc965e1abe982d06ab9ca636fcb
Nehani Gupta <nehani.gupta@gmail.com>
2022-10-25 14:33:09 +0530

    Assignment41_Nehanig

---
+++ src/nehaniG/Arraylist/Employee.java
@@ -0,0 +1,46 @@
+package nehaniG.Arraylist;
+
+class Employee implements Comparable<Employee> {
+
+	private String empName;
+	private int empId;
+	private int empSalary;
+	private int empDeptId;
+
+	public Employee(String empName, int empId, int empSalary, int empDeptId) {
+		super();
+		this.empName = empName;
+		this.empId = empId;
+		this.empSalary = empSalary;
+		this.empDeptId = empDeptId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public int getEmpSalary() {
+		return empSalary;
+	}
+
+	public int getEmpDeptId() {
+		return empDeptId;
+	}
+
+	@Override
+	public int compareTo(Employee o) {
+		if (this.empSalary != o.empSalary) {
+			return this.empSalary - o.empSalary;
+		} else if (!this.empName.equals(o.empName)) {
+			return this.empName.compareTo(o.empName);
+		} else if (this.empDeptId != o.empDeptId) {
+			return this.empDeptId - o.empDeptId;
+		} else {
+			return this.empId - o.empId;
+		}
+	}
+}
