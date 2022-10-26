5a265bc2f9d7b3423e47012b442831c24f1a5f66
Pranjali Vyavaharkar <vyavaharkarpranjali@gmail.com>
2022-10-26 18:08:44 +0530

    Assignment_42_Pranjali

---
+++ src/pranjaliV/assignment42/Employee.java
@@ -0,0 +1,52 @@
+//Employee class should have empName, empId, empSalary, empDeptId as parameters.
+
+package pranjaliV.assignment42;
+
+public class Employee {
+	
+	private String empName;
+	private int empId;
+	private int empSalary;
+	private int empDeptId;
+	
+	public Employee(String empName, int empId, int empSalary, int empDeptId) {
+		super();
+		this.empName = empName;
+		this.empId = empId;
+		this.empSalary =  empSalary;
+		this.empDeptId = empDeptId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public int getEmpSalary() {
+		return empSalary;
+	}
+
+	public void setEmpSalary(int empSalary) {
+		this.empSalary = empSalary;
+	}
+
+	public int getEmpDeptId() {
+		return empDeptId;
+	}
+
+	public void setEmpDeptId(int empDeptId) {
+		this.empDeptId = empDeptId;
+	}
+
+}
