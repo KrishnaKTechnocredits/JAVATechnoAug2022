3c37113f5c4e8555cf90a56a409b38f06a76c3b0
Deepak Borse <deepak.borse27@gmail.com>
2022-11-01 00:52:41 +0530

    Assignment39_DeepakB

--- src/deepakBorse/Assignments/Assignment39.java
+++ src/deepakBorse/Assignments/Assignment39.java
@@ -53,7 +53,7 @@ public class Assignment39 {
 
 	void getDupname(String[] names) {
 		System.out.println("\nProgram 1: Print all duplicate names from given Array");
-		System.out.println("Input String array: "+Arrays.toString(names));
+		System.out.println("Input String array: " + Arrays.toString(names));
 		HashSet<String> setOfName = new HashSet<String>();
 		System.out.print("Output: ");
 		for (String name : names) {
@@ -67,46 +67,47 @@ public class Assignment39 {
 
 	void getuniqueName(String[] listnames) {
 		System.out.println("\n\nProgram 2 : Return unique names from given Array");
-		System.out.println("Input String array: "+Arrays.toString(listnames));
+		System.out.println("Input String array: " + Arrays.toString(listnames));
 		HashSet<String> listofuniquename = new HashSet<String>(Arrays.asList(listnames));
-		System.out.println("Output :"+listofuniquename);
+		System.out.println("Output :" + listofuniquename);
 	}
 
 	void getuniquecharater(String name) {
 		System.out.println("\nProgram 3 : Write a program to return unique characters from given name");
-		System.out.println("Input :"+name);
+		System.out.println("Input :" + name);
 		HashSet<Character> characterset = new LinkedHashSet<Character>();
 
 		for (int index = 0; index < name.length(); index++) {
 			Character ch = name.charAt(index);
 			characterset.add(ch);
 		}
-		System.out.println("Output :"+characterset);
+		System.out.println("Output :" + characterset);
 	}
 
 //Program 4 : Remove duplicates from array containing numbers [insertion order].
 	void removedupnumber(Integer[] numset) {
 		System.out.println("\nProgram 4 : Remove duplicates from array containing numbers [insertion order]");
-		System.out.println("Input array: "+Arrays.toString(numset));
+		System.out.println("Input array: " + Arrays.toString(numset));
 		Set<Integer> setofnum = new LinkedHashSet<Integer>();
 		for (Integer number : numset) {
 			setofnum.add(number);
 		}
 
-		System.out.println("Output :"+setofnum);
+		System.out.println("Output :" + setofnum);
 	}
+
 //Program 5 : Remove duplicates from array containing numbers [natural order]
 	void removedupNaturalorder(Integer[] numset) {
 		System.out.println("\nProgram 5 :Remove duplicates from array containing numbers [natural order]");
-		System.out.println("Input array: "+Arrays.toString(numset));
+		System.out.println("Input array: " + Arrays.toString(numset));
 		Set<Integer> setofnum = new TreeSet<Integer>();
 		for (Integer number : numset) {
 			setofnum.add(number);
 		}
 
-		System.out.println("Output :"+setofnum);
+		System.out.println("Output :" + setofnum);
 	}
-	
+
 //Program 7 : Remove duplicates from array containing numbers using Set
 	Set<Integer> removeDuplicatesFromArrayUsingSet(Integer[] arr) {
 
@@ -114,27 +115,27 @@ public class Assignment39 {
 		Set<Integer> outputHashSet1 = new LinkedHashSet<>();
 
 		for (int num : arr) {
-			boolean flag=outputHashSet.add(num);
-			if(!flag) {
+			boolean flag = outputHashSet.add(num);
+			if (!flag) {
 				outputHashSet1.add(num);
 			}
 		}
 		return outputHashSet1;
 	}
-	
-	//Program 7 : Remove duplicates from array containing numbers using List.
-	List<Integer> removeDuplicateNumbersFromArray(Integer[] arr){
 
-	ArrayList<Integer> listOfNum = new ArrayList<>(Arrays.asList(arr));
-	ArrayList<Integer> outputList = new ArrayList<Integer>();
+	// Program 7 : Remove duplicates from array containing numbers using List.
+	List<Integer> removeDuplicateNumbersFromArray(Integer[] arr) {
 
-	for(Integer num:listOfNum) {
-		if(listOfNum.indexOf(num)!=listOfNum.lastIndexOf(num) && !outputList.contains(num)) {
-			outputList.add(num);
+		ArrayList<Integer> listOfNum = new ArrayList<>(Arrays.asList(arr));
+		ArrayList<Integer> outputList = new ArrayList<Integer>();
+
+		for (Integer num : listOfNum) {
+			if (listOfNum.indexOf(num) != listOfNum.lastIndexOf(num) && !outputList.contains(num)) {
+				outputList.add(num);
+			}
 		}
+		return outputList;
 	}
-	return outputList;
-}
 
 	public static void main(String[] args) {
 
@@ -152,14 +153,14 @@ public class Assignment39 {
 		Integer[] numberset = { 10, 10, 13, 12, 12, 10, 55, 66, 55, 11 };
 		Assignment.removedupnumber(numberset);
 		Assignment.removedupNaturalorder(numberset);
-		
+
 		System.out.println("\nProgram 6: Remove duplicates from array containing numbers using Set");
-		System.out.println("Input array: "+Arrays.toString(numberset));
-		System.out.println("Output: "+Assignment.removeDuplicatesFromArrayUsingSet(numberset));
+		System.out.println("Input array: " + Arrays.toString(numberset));
+		System.out.println("Output: " + Assignment.removeDuplicatesFromArrayUsingSet(numberset));
 
 		System.out.println("\nProgram 7: Remove duplicates from array containing numbers using List.");
-		System.out.println("Input array: "+Arrays.toString(numberset));
-		System.out.println("Output: "+Assignment.removeDuplicateNumbersFromArray(numberset));
+		System.out.println("Input array: " + Arrays.toString(numberset));
+		System.out.println("Output: " + Assignment.removeDuplicateNumbersFromArray(numberset));
 	}
 
 }
