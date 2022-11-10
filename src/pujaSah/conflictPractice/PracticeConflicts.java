fa574e9948a1816ced8d97ee6d595dee611a00f8
Puja Sah <pujasah5@gmail.com>
2022-11-10 08:42:53 +0530

    Conflict scenario1

---
+++ src/pujaSah/conflictPractice/PracticeConflicts.java
@@ -0,0 +1,13 @@
+package pujaSah.conflictPractice;
+
+public class PracticeConflicts {
+	
+	public static void main() {
+	System.out.println("Line 1");
+	System.out.println("Line 2");
+	System.out.println("Line 3");
+	System.out.println("Line 4");
+	System.out.println("Line 5");
+	System.out.println("Line 6");
+	}
+}
